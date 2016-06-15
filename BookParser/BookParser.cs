3c5ed047f1a392cd111aaa1f4acc6cf9f1f05072
lorenzoturrino <neuronefunzionante@gmail.com>
2016-06-15 10:46:24 +0100

    add Sample1 and console output of the parsing to main()

--- BookParser/BookParser.cs
+++ BookParser/BookParser.cs
@@ -10,7 +10,16 @@ namespace BookParser
     {
         static public void Main(string[] args)
         {
-            Console.WriteLine("sup");
+            Console.WriteLine("Starting up sample reading");
+
+            string filePath = "C:\\Users\\neuro\\OneDrive\\prowin\\BookParser\\Sample1.txt";
+
+            TextFile sampleFile = new TextFile(filePath);
+            Dictionary<string,int> occurrences = sampleFile.CountOccurrences();
+            foreach (KeyValuePair<string,int> entry in occurrences.OrderByDescending(entry => entry.Value))
+            {
+                Console.WriteLine("entry: " + entry.Key + " - " + entry.Value);
+            }
         }
     }
 }
