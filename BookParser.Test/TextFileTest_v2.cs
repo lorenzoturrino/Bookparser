7321fd1fc2308dce88efdaed2eaf50e3b8500f91
lorenzoturrino <neuronefunzionante@gmail.com>
2016-06-15 16:29:12 +0100

    create TextFile_2 and add constructor+rawText property

--- BookParser.Test/TextFileTest_v2.cs
+++ BookParser.Test/TextFileTest_v2.cs
@@ -4,9 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using NUnit;
+using NUnit.Framework;
+using BookParser;
+
 namespace BookParser.Test
 {
+    [TestFixture]
     class TextFileTest_v2
     {
+        string fileString = "This is a Test";
+        string filePath = @"C:\Users\neuro\OneDrive\prowin\BookParser\BookParser.Test\bin\TestFile.txt";
+        string inputString = "Hello Friend";
+
+        [TestCase]
+        public void Constructor_WithFile_DefaultArgument()
+        {
+            TextFile_v1 testFile = new TextFile_v1(filePath);
+            Assert.AreEqual(fileString, testFile.rawText);
+        }
+
+        [TestCase]
+        public void Constructor_WithFile_ExplicitArgument()
+        {
+            TextFile_v1 testFile = new TextFile_v1(filePath, true);
+            Assert.AreEqual(fileString, testFile.rawText);
+        }
+
+        [TestCase]
+        public void Constructor_WithString()
+        {
+            TextFile_v1 testFile = new TextFile_v1(inputString, false);
+            Assert.AreEqual(inputString, testFile.rawText);
+        }
     }
 }
--- BookParser/TextFile_v2.cs
+++ BookParser/TextFile_v2.cs
@@ -8,5 +8,28 @@ namespace BookParser
 {
     class TextFile_v2
     {
+        private string fileContent;
+        //private wordArray;
+        //private Dictionary<int, List<string>> sortedOccurrences;
+
+        public TextFile_v2(string inputString, bool isFile = true)
+        {
+            if (isFile)
+            {
+                fileContent = System.IO.File
+                .ReadAllText(inputString)
+                .TrimEnd('\r', '\n');
+            }
+            else
+            {
+                fileContent = inputString;
+            }
+
+        }
+
+        public string rawText
+        {
+            get { return fileContent; }
+        }
     }
 }
