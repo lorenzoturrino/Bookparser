aee0f67355b6bb6bab9287b48ab84c1288d8dd7c
lorenzoturrino <neuronefunzionante@gmail.com>
2016-06-16 14:32:38 +0100

    extract BookParser.PrintResultToConsole method

--- BookParser/BookParser.cs
+++ BookParser/BookParser.cs
@@ -10,22 +10,27 @@ namespace BookParser
     {
         static public void Main(string[] args)
         {
-            Console.WriteLine("Starting up sample reading");
-
-            string filePath = @"C:\Users\neuro\OneDrive\prowin\BookParser\Sample1.txt";
+            
+            string filePath = @"..\..\..\Sample1.txt";
+            Console.WriteLine("Starting up sample reading from:" + filePath);
 
             TextFile_v1 sampleFile = new TextFile_v1(filePath, true);
             Dictionary<int, List<string>> resultList = sampleFile.SortOccurrencesByNumber();
+            PrintResultToConsole(resultList);
 
-            foreach (KeyValuePair<int,List<string>> entry in resultList.OrderByDescending(pair => pair.Key))
+        }
+
+        static private void PrintResultToConsole(Dictionary<int, List<string>> blob)
+        {
+            foreach (KeyValuePair<int, List<string>> entry in blob.OrderByDescending(pair => pair.Key))
             {
                 string line = entry.Key.ToString();
-                line += (MathHelper.CheckPrimality(entry.Key) ? " (Prime number)" : " (Not a prime number)") + " appearances:\n";
+                line += (MathHelper.CheckPrimality(entry.Key) ? " (Prime number)" : " (Not a prime number)") + " appearances:\n| ";
                 foreach (string word in entry.Value)
                 {
-                    line += word + ", ";
+                    line += word + " | ";
                 }
-                line = line.Remove(line.Length - 2) + "\n\n";
+                line += "\n\n";
                 Console.Write(line);
             }
         }
